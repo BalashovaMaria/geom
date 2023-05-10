fd5ef93a74ad098d4c1c255736c0ed1dc2b2f311
BalashovaMaria <99802613+BalashovaMaria@users.noreply.github.com>
2023-05-10 23:21:09 +0300

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,23 @@
+import random
+from PIL import Image, ImageDraw #Подключим необходимые библиотеки
+
+image = Image.open("roof.jpg") #Открываем изображение
+draw = ImageDraw.Draw(image) #Создаем инструмент для рисования
+width = image.size[0] #Определяем ширину
+height = image.size[1] #Определяем высоту
+pix = image.load() #Выгружаем значения пикселей
+div = 200
+move = width // div
+k = 0
+for x in range(width):
+    if x % move == 0 and k < 1:
+        k += 2 / div
+    elif x % move == 0 and k >= 1:
+        k += 10 / div
+    for y in range(height):
+        a = pix[x, y][0]
+        b = pix[x, y][1]
+        c = pix[x, y][2]
+        draw.point((x, y), (int(a * k), int(b * k), int(c * k)))
+image.show()
+del draw
